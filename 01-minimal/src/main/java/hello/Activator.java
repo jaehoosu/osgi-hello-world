8b7750035494c8e55e8d10026f0d27c11e87a826
Piotr Paczynski <github@pitek.eu.org>
2015-11-29 01:32:10 +0100

    Simplified Activator and configured launch config to stop the framework

--- 01-minimal/src/main/java/hello/Activator.java
+++ 01-minimal/src/main/java/hello/Activator.java
@@ -2,13 +2,11 @@ package hello;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
 
 public class Activator implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
 		System.out.println("Hello!");
-		bundleContext.getBundle(Constants.SYSTEM_BUNDLE_ID).stop();
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
