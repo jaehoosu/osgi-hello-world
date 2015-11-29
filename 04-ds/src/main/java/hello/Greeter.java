0b2ef2a5c6737a7ebd20eef00639d0ce12c043ec
Piotr Paczynski <github@pitek.eu.org>
2015-11-29 12:37:58 +0100

    04: Added ds component

---
+++ 04-ds/src/main/java/hello/Greeter.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Greeter {
+
+	public void activate() throws Exception {
+		System.out.println("Hello!");
+	}
+
+	public void deactivate() throws Exception {
+		System.out.println("Bye...");
+	}
+
+}
