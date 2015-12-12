ea8ed6e5f6b970e249c539131500888b6e0f1e71
Piotr Paczynski <github@pitek.eu.org>
2015-12-12 13:41:45 +0100

    added @QueryParam injection

--- 06-servlet/src/hello/Greeter.java
+++ 06-servlet/src/hello/Greeter.java
@@ -2,16 +2,17 @@ package hello;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 import org.osgi.service.component.annotations.Component;
 
 @Path("/greeter")
-@Component(service = Object.class)
+@Component(service = Object.class, servicefactory = true)
 public class Greeter {
 
 	@GET
-	public String greet() throws Exception {
-		return "Hello!";
+	public String greet(@QueryParam(value = "name") String name) throws Exception {
+		return "Hello " + name;
 	}
 
 }
